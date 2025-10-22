8d3e27f1b31f7379213deccaf30175f474e27c23
Angel Santos <93794342+angeldeyisus@users.noreply.github.com>
2025-10-21 20:37:11 -0700

    Implementacion inicio sesion / registro
    Se implementó la función de iniciar sesión y registrarse, además de guardar los datos del usuario al firebase

--- ProyectoFinal_Eq1_GestorDeGastosPersonales/app/src/main/java/alcala/jose/proyectofinal_eq1_gestordegastospersonales/IniciarSesion.kt
+++ ProyectoFinal_Eq1_GestorDeGastosPersonales/app/src/main/java/alcala/jose/proyectofinal_eq1_gestordegastospersonales/IniciarSesion.kt
@@ -3,12 +3,19 @@ package alcala.jose.proyectofinal_eq1_gestordegastospersonales
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
 
 class IniciarSesion : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,13 +25,43 @@ class IniciarSesion : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        auth = Firebase.auth
         val btnIngresarInicio: Button = findViewById(R.id.btnIngresarInicio)
+        val correo: EditText = findViewById(R.id.etCorreo)
+        val contraseña: EditText = findViewById(R.id.etConfirmarContraseña)
 
         btnIngresarInicio.setOnClickListener {
+            if (correo.text.toString().isEmpty() || contraseña.text.toString().isEmpty()){
+                showError("Todos los campos son obligatorios")
+            }else{
+                iniciarSesion(correo.text.toString(), contraseña.text.toString())
+            }
+        }
+    }
 
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
+    fun goToMain(user: FirebaseUser){
+        val intent = Intent(this, HomeActivity::class.java)
+        intent.putExtra("user", user.email)
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    fun showError(text: String){
+        Toast.makeText(
+            baseContext,
+            text,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    fun iniciarSesion(correo: String, contraseña: String){
+        auth.signInWithEmailAndPassword(correo, contraseña).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                val user = auth.currentUser
+                goToMain(user!!)
+            }else{
+                showError("Correo o contraseña incorrectos")
+            }
         }
     }
 }
--- ProyectoFinal_Eq1_GestorDeGastosPersonales/app/src/main/java/alcala/jose/proyectofinal_eq1_gestordegastospersonales/Registro.kt
+++ ProyectoFinal_Eq1_GestorDeGastosPersonales/app/src/main/java/alcala/jose/proyectofinal_eq1_gestordegastospersonales/Registro.kt
@@ -1,12 +1,24 @@
 package alcala.jose.proyectofinal_eq1_gestordegastospersonales
 
+import alcala.jose.proyectofinal_eq1_gestordegastospersonales.entidades.Usuario
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.database.FirebaseDatabase
 
 class Registro : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private val usuarioRef = FirebaseDatabase.getInstance().getReference("usuarios")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +28,90 @@ class Registro : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        auth = Firebase.auth
+        val nombre: EditText = findViewById(R.id.etNombre)
+        val apellido: EditText = findViewById(R.id.etApellido)
+        val correo: EditText = findViewById(R.id.etCorreo)
+        val contraseña: EditText = findViewById(R.id.etContraseña)
+        val confirmarContraseña: EditText = findViewById(R.id.etConfirmarContraseña)
+        val fechaNacimiento: EditText = findViewById(R.id.etFechaNacimiento)
+        val botonRegistro: Button = findViewById(R.id.btnRegistrarGasto)
+
+        botonRegistro.setOnClickListener {
+            if (!verificarCampos()) {
+                Log.w("ERROR", "RegistroConCorreo:falló")
+                Toast.makeText(
+                    baseContext,
+                    "Todos los campos son obligatorios",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else if (!contraseña.text.toString().equals(confirmarContraseña.text.toString())){
+                Log.w("ERROR", "RegistroConCorreo:falló")
+                Toast.makeText(
+                    baseContext,
+                    "Las contraseñas no coinciden",
+                    Toast.LENGTH_LONG
+                ).show()
+            }else {
+                registrarse(correo.text.toString(), contraseña.text.toString())
+                guardarUsuario()
+            }
+        }
+    }
+
+    fun registrarse(correo: String, contraseña: String){
+        Log.d("INFO", "email: ${correo}, contraseña: ${contraseña}")
+        auth.createUserWithEmailAndPassword(correo, contraseña).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                Log.d("INFO", "RegistroConCorreo:exitoso")
+                val usuario = auth.currentUser
+                val intent = Intent(this, IniciarSesion::class.java)
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }else {
+                Log.w("ERROR", "RegistroConCorreo:falló", task.exception)
+                Toast.makeText(
+                    baseContext,
+                    "El registro falló",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun guardarUsuario() {
+        val nombre: EditText = findViewById(R.id.etNombre)
+        val apellido: EditText = findViewById(R.id.etApellido)
+        val correo: EditText = findViewById(R.id.etCorreo)
+        val fechaNacimiento: EditText = findViewById(R.id.etFechaNacimiento)
+
+        val usuario = Usuario(
+            nombre.text.toString(),
+            apellido.text.toString(),
+            correo.text.toString(),
+            fechaNacimiento.text.toString()
+        )
+        usuarioRef.push().setValue(usuario)
+    }
+
+    fun verificarCampos(): Boolean {
+        val nombre: EditText = findViewById(R.id.etNombre)
+        val apellido: EditText = findViewById(R.id.etApellido)
+        val correo: EditText = findViewById(R.id.etCorreo)
+        val contraseña: EditText = findViewById(R.id.etContraseña)
+        val confirmarContraseña: EditText = findViewById(R.id.etConfirmarContraseña)
+        val fechaNacimiento: EditText = findViewById(R.id.etFechaNacimiento)
+        if (nombre.text.toString().isEmpty()
+            || apellido.text.toString().isEmpty()
+            || correo.text.toString().isEmpty()
+            || contraseña.text.toString().isEmpty()
+            || confirmarContraseña.text.toString().isEmpty()
+            || fechaNacimiento.text.toString().isEmpty()){
+            return false
+        } else {
+            return true
+        }
+
     }
 }
